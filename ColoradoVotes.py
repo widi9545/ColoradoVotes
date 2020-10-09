6db3c40a8c5cb41bf460da82547056ca2745a0f1
widi9545 <widi9545@colorado.edu>
2020-10-08 21:06:50 -0600

    adding new folder

--- ColoradoVotes.py
+++ ColoradoVotes.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Author - William Diment
+Email: william.j.diment@gmail.com preferred, otherwise widi9545@colorado.edu
+Project Begin Date 10/8/20
+Purpose: Develop a website showing reported Colorado voter ballot returns for 2020 General Election according to
+county and party affiliation 
+
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output, State
+from sys import stdout 
+import datetime
+import os
+import pandas as pd
+import numpy
+import plotly.express as px
+
+
+
+
+
+
 
 
 
